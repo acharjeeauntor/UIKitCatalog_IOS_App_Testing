01899b48695944e41ff17d2efc01fe37a91be658
Auntor Acharja <auntoracharja@Auntors-MacBook-Air.local>
2022-09-07 22:46:04 +0600

    Added Alert popup related testcase automation

--- UIKitCatalog_Automation_Script/test/screenobjects/homescreen.js
+++ UIKitCatalog_Automation_Script/test/screenobjects/homescreen.js
@@ -1,12 +1,17 @@
 class HomeScreen {
     activityIndicatorSelector=`~Activity Indicators`
     searchSelector=`~Search`
+    alertViewsSelector = `~Alert Views`
 
+    
     async clickActivityIndicatortBtn(){
         await $(this.activityIndicatorSelector).click() 
     }
     async clickSearchBtn(){
         await $(this.searchSelector).click() 
+    }
+    async clickAlertViewsBtn(){
+        await $(this.alertViewsSelector).click() 
     }
 }
 
